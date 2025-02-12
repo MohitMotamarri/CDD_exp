a62fc8fa4685c7060aaa1483e0b3867b251a400f
MohitMotamarri <mohitmotamarri@gmail.com>
2025-02-12 10:44:00 +0530

    commited

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-a=1
-b=2
-sum=a+b
-print('sum of num is{0} and [D[D[D[D[D[D[D[1[C[C[C[C[C[C{2}'.format(a,[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[0[C[C[C[C[C[C[C[1[C is {2}[C[C['.format(a,b,sum)
+a = 1
+b = 2
+sum = a + b
+print("Sum of num is", sum)
